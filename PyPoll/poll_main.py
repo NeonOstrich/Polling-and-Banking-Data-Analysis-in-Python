84170032cda8408ea40bf1135e60bfc079662b9d
NeonOstrich <oliver.zagorin@gmail.com>
2023-01-07 13:58:08 -0500

    Jan 7 2023

--- PyPoll/poll_main.py
+++ PyPoll/poll_main.py
@@ -1,12 +1,27 @@
+import os
 import csv
 
-P = open('/Users/oliverzagorin/Desktop/GW_Bootcamp/Homework/python-challenge/PyPoll/Resources/election_data.csv')
+# Set path for file
+csvpath = os.path.join("..", "Resources", "election_data.csv")
 
-csv_P = csv.reader(P)
-Poll_data = []
+total_votes = 0
+candidate_list = []
 
-for row in csv_P:
 
-    print(len(row))
+# Open the CSV
+with open(csvpath) as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=",")
 
-P.close()
+    # Loop through updating variable values
+    for row in csvreader:
+
+        total_votes == total_votes +1
+
+
+
+
+    print(f"Total Votes: int{total_votes}")
+    print(f"str{candidate_list}")
+    print(f"Average Change: {change/total_months}")
+    print(f"Greatest Increase in Profits: {increase_date} ({greatest_increase})")
+    print(f"Greatest Increase in Profits: {decrease_date} ({greatest_decrease})")
