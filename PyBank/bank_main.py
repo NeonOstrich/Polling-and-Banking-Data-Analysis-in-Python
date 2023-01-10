416c83f5a89cf477b19e03f75db47ad245f51ab4
NeonOstrich <oliver.zagorin@gmail.com>
2023-01-10 12:49:51 -0500

    Jan 10 2023

--- PyBank/bank_main.py
+++ PyBank/bank_main.py
@@ -2,43 +2,51 @@ import os
 import csv
 
 # Set path for file
-csvpath = os.path.join("..", "Resources", "budget_data.csv")
+csvpath = os.path.join("Resources", "budget_data.csv")
 
 total_months = 0
 total_pc = 0
 change = 0
 greatest_increase = 0
 increase_date = 0
-greatest_decrease = 0
+greatest_decrease = 100000000000000
 decrease_date = 0
 
 # Open the CSV
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    header = next(csvreader)
 
     # Loop through updating variable values
     for row in csvreader:
+        total_months += 1
 
-        total_months == total_months +1
+        total_pc += int(row[1])
+        if total_months > 1:
+            change += int(row[1]) - previous_bl
+       
 
-        total_pc == total_pc + int(row, 2)
+            if int(row[1]) - previous_bl > greatest_increase:
+                greatest_increase = int(row[1]) - previous_bl
+                increase_date = (str(row[0]))
+            
+            if int(row[1]) - previous_bl < greatest_decrease:
+                greatest_decrease = int(row[1]) - previous_bl
+                decrease_date = (str(row[0]))
 
-        change == change + abs(int(row,2) - int(row-1,2))
+        previous_bl = int(row[1])
 
-        if (int(row,2) - int(row-1,2)) > greatest_increase:
-            greatest_increase == (int(row,2) - int(row-1,2))
-            increase_date == (str(row,1))
-        
-        elif (int(row,2) - int(row-1,2)) < greatest_decrease:
-            greatest_decrease == (int(row,2) - int(row-1,2))
-            decrease_date == (str(row,1))
+    output = (f"Financial Analysis\n"
+  f"----------------------------\n"
+        f"Total Months: {total_months}\n"
+f"Total: ${total_pc}\n"
+f"Average Change: ${round(change/(total_months-1),2)}\n"
+f"Greatest Increase in Profits: {increase_date} (${greatest_increase})\n"
+f"Greatest Decrease in Profits: {decrease_date} (${greatest_decrease})")
 
-    print(f"Total Months: {total_months}")
-    print(f"Total: {total_pc}")
-    print(f"Average Change: {change/total_months}")
-    print(f"Greatest Increase in Profits: {increase_date} ({greatest_increase})")
-    print(f"Greatest Increase in Profits: {decrease_date} ({greatest_decrease})")
-    
+print(output)
 
+output_path = os.path.join("analysis", "financial_analysis.txt")
 
-    # If the book is never found, alert the user
+with open(output_path, "w") as output_text:
+    output_text.write(output)
--- PyPoll/poll_main.py
+++ PyPoll/poll_main.py
@@ -2,26 +2,35 @@ import os
 import csv
 
 # Set path for file
-csvpath = os.path.join("..", "Resources", "election_data.csv")
+csvpath = os.path.join("Resources", "election_data.csv")
 
 total_votes = 0
 candidate_list = []
+candidate_dictionary = {}
 
 
 # Open the CSV
 with open(csvpath) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
+    
+    header = next(csvreader)
 
     # Loop through updating variable values
     for row in csvreader:
 
-        total_votes == total_votes +1
+        total_votes = total_votes +1
+
+        candidate_name = row[2]
+        if candidate_name not in candidate_list:
+            candidate_list.append(candidate_name)
+            candidate_dictionary[candidate_name] = 0
+
+        candidate_dictionary[candidate_name] +=1
+
+    percentage_dict = {key: value / total_votes for key, value in candidate_dictionary.items()}
 
 
-
-
-    print(f"Total Votes: int{total_votes}")
-    print(f"str{candidate_list}")
-    print(f"Average Change: {change/total_months}")
-    print(f"Greatest Increase in Profits: {increase_date} ({greatest_increase})")
-    print(f"Greatest Increase in Profits: {decrease_date} ({greatest_decrease})")
+    print(f"Total Votes: {total_votes}")
+    print(f"{candidate_list}")
+    print(f"Average Change: {candidate_dictionary}")
+    print(percentage_dict)
